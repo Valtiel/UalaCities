1b48778c7ba11d5338ce83085637543ae096a254
César Rosales <augusto.valtiel@gmail.com>
2025-08-29 13:46:13 -0300

    Initial Commit

---
+++ UalaCities/UalaCitiesApp.swift
@@ -0,0 +1,32 @@
+//
+//  UalaCitiesApp.swift
+//  UalaCities
+//
+//  Created by César Rosales on 29/08/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct UalaCitiesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ UalaCitiesTests/UalaCitiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  UalaCitiesTests.swift
+//  UalaCitiesTests
+//
+//  Created by César Rosales on 29/08/2025.
+//
+
+import Testing
+@testable import UalaCities
+
+struct UalaCitiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
